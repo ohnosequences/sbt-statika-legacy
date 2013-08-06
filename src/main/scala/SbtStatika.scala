e5068b028c4460105e4f1162e8b652fd10c7a9c4
Alexey Alekhin <laughedelic@gmail.com>
2013-08-06 17:39:41 +0200

    Added private resolvers setting (+in metadata)

--- src/main/scala/SbtStatika.scala
+++ src/main/scala/SbtStatika.scala
@@ -27,15 +27,19 @@ object SbtStatika extends Plugin {
     "statika library version")
 
   lazy val statikaResolvers = SettingKey[Seq[S3Resolver]]("statika-resolvers",
-    "resolvers for statika dependencies")
+    "Resolvers for statika dependencies")
+
+  lazy val statikaPrivateResolvers = SettingKey[Seq[S3Resolver]]("statika-private-resolvers",
+    "Private resolvers for statika dependencies")
 
   case class S3Bucket(url: String) {
     override def toString = url
+    def toHttp = if(url.startsWith("s3://"))
+         "http://"+ url.stripPrefix("s3://") +".s3.amazonaws.com"
+      else url
   }
-  def s3(url: String): S3Bucket = 
-    if(url.startsWith("s3://")) 
-      S3Bucket("http://"+ url.stripPrefix("s3://") +".s3.amazonaws.com")
-    else S3Bucket(url)
+  // short alias
+  def s3(url: String): S3Bucket = S3Bucket(url)
 
   case class S3Resolver(name: String, bucket: S3Bucket) {
     override def toString = """\"%s\" at \"%s\" """ format (name, bucket)
@@ -48,7 +52,8 @@ object SbtStatika extends Plugin {
   implicit def StringAtS3(s: String) = new StringAtS3(s)
   
   // convertion to Resolver
-  implicit def s3ToMvn(s3: S3Resolver): Resolver = s3.name at s3.bucket.toString
+  implicit def s3ToMvn(s3: S3Resolver): Resolver = s3.name at s3.bucket.toHttp
+
 
   override def settings = 
     startScriptForClassesSettings ++
@@ -57,35 +62,30 @@ object SbtStatika extends Plugin {
     Seq(
     // resolvers
 
-      statikaResolvers := Seq(
+      resolvers ++= Seq ( 
+        Resolver.typesafeRepo("releases")
+      , Resolver.sonatypeRepo("releases")
+      , Resolver.sonatypeRepo("snapshots")
+      )
+
+    , statikaResolvers := Seq(
         "Era7 Releases"  at s3("s3://releases.era7.com")
       , "Era7 Snapshots" at s3("s3://snapshots.era7.com")
-      , "Statika public snapshots" at s3("s3://snapshots.statika.ohnosequences.com")
       , "Statika public releases" at s3("s3://releases.statika.ohnosequences.com")
+      , "Statika public snapshots" at s3("s3://snapshots.statika.ohnosequences.com")
       )
 
     , resolvers <++= statikaResolvers { _ map s3ToMvn }
 
-    , resolvers ++= Seq ( 
-        // maven:
-        Resolver.typesafeRepo("releases")
-      , Resolver.sonatypeRepo("releases")
-      , Resolver.sonatypeRepo("snapshots")
-        // ivy:
-      // , Resolver.url("Era7 ivy snapshots", url("http://snapshots.era7.com.s3.amazonaws.com"))(Resolver.ivyStylePatterns)
-      // , Resolver.url("Era7 ivy releases",  url("http://releases.era7.com.s3.amazonaws.com"))(Resolver.ivyStylePatterns)
-      // , Resolver.url("Statika ivy public snapshots", url("http://snapshots.statika.ohnosequences.com.s3.amazonaws.com"))(Resolver.ivyStylePatterns)
-      // , Resolver.url("Statika ivy public releases",  url("http://releases.statika.ohnosequences.com.s3.amazonaws.com"))(Resolver.ivyStylePatterns)
-      )
-
     // private resolvers
 
-    , resolvers <++= s3credentials { cs => Seq(
-          cs map s3resolver("Statika private snapshots", "s3://private.snapshots.statika.ohnosequences.com")
-        , cs map s3resolver("Statika private releases",  "s3://private.releases.statika.ohnosequences.com")
-        // , cs map s3resolver("Statika ivy private snapshots", "s3://private.snapshots.statika.ohnosequences.com", Resolver.localBasePattern)
-        // , cs map s3resolver("Statika ivy private releases",  "s3://private.releases.statika.ohnosequences.com", Resolver.localBasePattern)
-        ).flatten 
+    , statikaPrivateResolvers := Seq(
+        "Statika private releases"  at s3("s3://private.releases.statika.ohnosequences.com")
+      , "Statika private snapshots" at s3("s3://private.snapshots.statika.ohnosequences.com")
+      )
+
+    , resolvers <++= (s3credentials, statikaPrivateResolvers) { (cs, rs) => 
+        rs map { r => cs map s3resolver(r.name, r.bucket.toString) } flatten
       }
 
     // publishing
@@ -122,7 +122,6 @@ object SbtStatika extends Plugin {
         Seq (
           "com.chuusai" %% "shapeless" % "1.2.+"
         , "ohnosequences" %% "statika" % sv
-        // , "ohnosequences" % "gener8bundle_2.10.0" % "0.12.+" % "test"
         , "org.scalatest" %% "scalatest" % "1.9.1" % "test"
         )
       }
@@ -136,25 +135,23 @@ object SbtStatika extends Plugin {
           (bp, bo, bi)  =>  
         if (bp.isEmpty || bo.isEmpty) Seq() else Seq(bi)
       }
-    , buildInfoKeys <<= (name, bundlePackage, bundleObject, statikaResolvers) { 
-        (name, pkg, obj, sResolvers) =>
+    , buildInfoKeys <<= (name, bundlePackage, bundleObject, statikaResolvers, statikaPrivateResolvers) { 
+        (name, pkg, obj, sResolvers, sPrivResolvers) =>
         Seq[BuildInfoKey](
           organization
         , "artifact" -> name
         , version
-        // , s3credentialsFile
-        // , statikaVersion
+        , statikaVersion
         , "name" -> (pkg+"."+obj)
         , "resolvers" -> sResolvers
+        , "privateResolvers" -> sPrivResolvers
         )
       }
     , buildInfoPackage <<= bundlePackage { _+".meta"}
-    // , buildInfoPrefix := "object  {"
     , buildInfoObjectFormat <<= (bundlePackage, bundleObject) { (bp, bo) =>
         "object %s extends ohnosequences.statika.MetaData.MetaDataOf["+bp+"."+bo+".type]"
       }
     , buildInfoObject <<= bundleObject
-    // , buildInfoSuffix := "}"
     ) 
 
     // sbt-release plugin
